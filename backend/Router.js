60b9ef585f378cf87e0a751d0068078e6664c248
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-04-15 21:55:58 +0700

    Add logout and isloggedin routes

--- backend/Router.js
+++ backend/Router.js
@@ -67,8 +67,51 @@ class Router {
     });
   }
 
-  logout(app, db) {}
-  isLoggedIn(app, db) {}
+  logout(app, db) {
+    app.post('/logout', (req, res) => {
+      if ((req, session.userID)) {
+        req.session.destroy();
+        res.json({
+          success: true,
+        });
+
+        return true;
+      } else {
+        res.json({
+          success: false,
+        });
+        return false;
+      }
+    });
+  }
+  isLoggedIn(app, db) {
+    app.post('/isLoggedIn', (req, res) => {
+      if (req.session.userID) {
+        let cols = [req.session.userID];
+        db.query(
+          'SELECT * FROM user WHERE id = ? LIMIT 1',
+          cols,
+          (err, data, fields) => {
+            if (data && data.length === 1) {
+              res.json({
+                success: true,
+                username: data[0].username,
+              });
+              return true;
+            } else {
+              res.json({
+                success: false,
+              });
+            }
+          }
+        );
+      } else {
+        res.json({
+          success: false,
+        });
+      }
+    });
+  }
 }
 
 module.exports = Router;
